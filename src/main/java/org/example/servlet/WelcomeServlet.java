bca8fab76f929e48134077b7840da0df335574fe
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2023-06-09 18:58:41 +0300

    project init

---
+++ src/main/java/org/example/servlet/WelcomeServlet.java
@@ -0,0 +1,20 @@
+package org.example.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.example.TemplateEngineUtil;
+
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+
+        TemplateEngineUtil.render("index.html", request, response);
+    }
+}

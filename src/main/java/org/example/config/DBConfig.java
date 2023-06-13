c7f8798116f1d99f429590ac46686428f9f2100e
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2023-06-13 12:26:57 +0300

    add DBConfig

--- src/main/java/org/example/config/DBConfig.java
+++ src/main/java/org/example/config/DBConfig.java
@@ -19,4 +19,57 @@ public class DBConfig {
 
     private String password;
 
+    public DBConfig() {
+        this.driver = "org.postgresql.Driver";
+        this.url = "jdbc:postgresql://localhost:5432/supermarket";
+        this.username = "postgres";
+        this.password = "postgres";
+    }
+
+    public DBConfig(String test) {
+        if (test.equals("test")) {
+            this.driver = "org.h2.Driver";
+            this.url = "jdbc:h2:./database";
+            this.username = "sa";
+            this.password = "sa";
+        }
+    }
+
+    public final Connection getConnection() {
+        Connection connection = null;
+        try {
+            try {
+                Class.forName(driver);
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static String getFileContent(String fileName) throws IOException {
+        Path pathToSolution = Paths.get(fileName).toAbsolutePath();
+        return Files.readString(pathToSolution).trim();
+    }
+
+    public static void initForTest(Connection connection) throws SQLException, IOException {
+        if (connection != null) {
+            Statement statement = connection.createStatement();
+            String initSql = getFileContent("src/test/resources/seed.sql");
+            statement.execute(initSql);
+        }
+    }
 }
--- src/main/java/org/example/dao/CartDao.java
+++ src/main/java/org/example/dao/CartDao.java
@@ -15,4 +15,117 @@ import java.util.List;
 
 public final class CartDao {
 
+    private DBConfig config;
+    private Statement statement;
+    private PreparedStatement preparedStatement;
+
+    public CartDao(DBConfig config) {
+        this.config = config;
+    }
+
+    public List<CartDto> getAll() {
+
+        List<CartDto> list = new ArrayList<>();
+
+        Connection connection = config.getConnection();
+
+        try {
+
+            statement = connection.createStatement();
+            String sql = "SELECT * FROM carts ORDER BY id;";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                CartDto cartDto = new CartDto();
+                cartDto.setId(resultSet.getInt("id"));
+                cartDto.setDescription(resultSet.getString("description"));
+                list.add(cartDto);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    DBConfig.closeConnection(connection);
+                }
+            } catch (SQLException e) {
+                System.out.println("SQLException");
+            }
+        }
+        return list;
+    }
+
+    public Cart getById(long id) {
+        Cart cart = new Cart();
+
+        Connection connection = config.getConnection();
+
+        try {
+
+            String sqlFindPCart = "SELECT * FROM carts WHERE id = ?;";
+
+
+            preparedStatement = connection.prepareStatement(sqlFindPCart);
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                cart = new Cart();
+                cart.setId(resultSet.getInt("id"));
+                cart.setDescription((resultSet.getString("description")));
+            }
+
+
+
+    public void save(String name) {
+
+        Connection connection = config.getConnection();
+
+        try {
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    DBConfig.closeConnection(connection);
+                }
+            } catch (SQLException e) {
+                System.out.println("SQLException during close(): " + e.getMessage());
+            }
+        }
+    }
+
+    public void delete(long id) {
+
+        Connection connection = config.getConnection();
+
+        try {
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+
+        }
+    }
+
+    public void update(long id, String name) {
+
+        Connection connection = config.getConnection();
+
+        try {
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+
+        }
+    }
 }
